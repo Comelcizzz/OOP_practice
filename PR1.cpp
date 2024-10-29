bd8ccc70bb7c5657f3deda9cc748a2336a2ae19e
Comelcizzz <ivanwarzp@gmail.com>
2024-10-28 23:59:26 -0400

    overload writing object to std::cin

--- PR1.cpp
+++ PR1.cpp
@@ -1,7 +1,12 @@
 ﻿#include "Student.h"
-
+#include <Windows.h>
+        
 int main()
-{
+{   
+    SetConsoleOutputCP(1251);
+
     Student object1, object2, object3; 
     cin >> object1 >> object2 >> object3;
+
+    cout << object1 << endl << object2 << endl << object3;
 }
